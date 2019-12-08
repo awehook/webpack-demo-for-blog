fcfa55313ccf4447566dc5ebeb8fb4e495fa4dc3
awehook <awehook@163.com>
2019-12-08 10:25:14 +0800

    add html-webpack-plugin

--- basic-demo/src/index.js
+++ basic-demo/src/index.js
@@ -1,4 +1,5 @@
-import Styles from 'style-loader!css-loader?modules!./demo.css';
+// import Styles from 'style-loader!css-loader?modules!./demo.css';
+import demo2 from './demo2.css';
 
-
-console.log(Styles);
+// console.log(Styles);
+console.log(demo2);
--- basic-demo/webpack.config.js
+++ basic-demo/webpack.config.js
@@ -1,4 +1,5 @@
 const path = require("path");
+const HtmlWebpackPlugin = require("html-webpack-plugin");
 
 const resolveApp = p => path.resolve(__dirname, p);
 module.exports = {
@@ -9,5 +10,14 @@ module.exports = {
   output: {
     path: resolveApp("build"),
     filename: "static/js/[name].[contenthash:8].js"
+  },
+  plugins: [new HtmlWebpackPlugin()],
+  module: {
+    rules: [{ test: /\.css/, use: [ "style-loader","css-loader"] }]
+  },
+  devServer: {
+    contentBase: resolveApp("build"),
+    compress: true,
+    port: 9000
   }
 };
