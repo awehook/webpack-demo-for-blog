0ebc528f85df0ad3abbb6b3eca9a1f6eeb45715a
awehook <awehook@163.com>
2019-12-08 15:17:35 +0800

    add webpack demo

--- basic-demo/webpack.config.js
+++ basic-demo/webpack.config.js
@@ -1,7 +1,9 @@
 const path = require("path");
 const HtmlWebpackPlugin = require("html-webpack-plugin");
+const MiniCssExtractPlugin = require("mini-css-extract-plugin");
 
 const resolveApp = p => path.resolve(__dirname, p);
+const appHtml = resolveApp("public/index.html");
 module.exports = {
   mode: "production",
   entry: {
@@ -11,9 +13,34 @@ module.exports = {
     path: resolveApp("build"),
     filename: "static/js/[name].[contenthash:8].js"
   },
-  plugins: [new HtmlWebpackPlugin()],
+  plugins: [
+    new HtmlWebpackPlugin({
+      inject: true,
+      template: appHtml
+    })
+  ],
   module: {
-    rules: [{ test: /\.css/, use: [ "style-loader","css-loader"] }]
+    rules: [
+      {
+        test: /\.css/,
+        use: [
+          "style-loader",
+          // {
+          //   loader: MiniCssExtractPlugin.loader,
+          //   options: {
+          //     // publicPath: "./public/"
+          //   }
+          // },
+          {
+            loader: "css-loader",
+            options: {
+              modules: true
+              // localIdentName: '[name]--[local]--[hash:base64:5]'
+            }
+          }
+        ]
+      }
+    ]
   },
   devServer: {
     contentBase: resolveApp("build"),
